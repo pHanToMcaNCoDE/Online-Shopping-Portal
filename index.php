fbbab50c28d7ecd598ace25b3d102494afbbeada
Victory Odumeh <vodumeh@gmail.com>
2023-06-21 08:24:41 +0100

    unnecessary file

--- index.php
+++ index.php
@@ -136,6 +136,28 @@
                         </div>
                     </div>
                 </div>
+                <div class="box">
+                    <div class="top">
+                        <div class="left">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round" class="feather feather-star">
+                                <polygon
+                                    points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+                                </polygon>
+                            </svg>
+                        </div>
+                        <div class="right">
+                            <p>Brands</p>
+                            <h4>0</h4>
+                        </div>
+                    </div>
+                    <div class="bottom">
+                        <div class="box-link">
+                            <a href="#">view more<i class="fa-solid fa-arrow-right-long fa-beat"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div class="table">
