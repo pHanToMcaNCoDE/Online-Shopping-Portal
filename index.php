75dc746b72562e285f27afcee1c67f60658c1dee
Victory Odumeh <vodumeh@gmail.com>
2023-06-29 22:20:12 +0100

    more and more corrections

--- index.php
+++ index.php
@@ -145,7 +145,7 @@
             <main role="main">
                 <div class="header">
                     <h2>
-                        <a href="#">
+                        <a href="index.php">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="feather feather-grid">
