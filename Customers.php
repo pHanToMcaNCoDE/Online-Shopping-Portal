56217887d2ca2f137ce60b25ae1c00351f9c5834
Victory Odumeh <vodumeh@gmail.com>
2023-06-30 17:28:51 +0100

    .

--- Customers.php
+++ Customers.php
@@ -98,7 +98,7 @@
                             </svg>
                             <a href="Orders.php">Orders</a>
                         </li>
-                        <li>
+                        <li class="current">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="feather feather-user">
