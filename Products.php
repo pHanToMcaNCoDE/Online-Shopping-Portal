7ca6cf9ee92b1e701309a6ae06033f36f3c09b46
Victory Odumeh <vodumeh@gmail.com>
2023-06-30 17:29:24 +0100

    .

--- Products.php
+++ Products.php
@@ -75,7 +75,7 @@
                             </svg>
                             <a href="Brands.php">Brands</a>
                         </li>
-                        <li>
+                        <li class="current">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="feather feather-gift">
