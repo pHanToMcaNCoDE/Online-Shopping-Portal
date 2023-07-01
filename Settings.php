297a86b243cfa8b93fd16d0b0f08cd780ef3166d
Victory Odumeh <vodumeh@gmail.com>
2023-07-01 21:00:15 +0100

    starting with settings

--- Settings.php
+++ Settings.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Portal || Products</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/settings.css">
     <!-- <link rel="stylesheet" href="assets/sweetalert2/sweetalert2.min.css"> -->
     <script src="https://kit.fontawesome.com/032421aa45.js" crossorigin="anonymous"></script>
 </head>
@@ -37,9 +37,9 @@
         </nav>
     </header>
 
-    <section class="product">
+    <section class="settings">
         <div class="max-width">
-            <aside style="width: 35%; height: 100vh;">
+            <aside style="width: 23%; height: 100vh;">
                 <div class=" x">
                     <svg class="x" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
                         fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
@@ -72,101 +72,44 @@
                             <a href="Brands.php">Password</a>
                         </li>
                         <li>
-                            <i class="fa-solid fa-arrow-right-from-bracket fa-flip" style="color: var(--w);"></i>
+                            <i class="fa-solid fa-arrow-right-from-bracket fa-flip"></i>
                             <a href="#">Sign-out</a>
                         </li>
                     </ul>
                 </div>
             </aside>
-            <main class="pdt" role="main">
-
+            <main class="settings" role="main">
                 <div class="deco">
                     <i class="fa-solid fa-clipboard"></i>
                 </div>
                 <div class="header">
                     <h2>
                         <a href="Products.php">
-                            <i class="fa-solid fa-arrow-right-from-bracket fa-flip"></i>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="feather feather-gift">
+                                <polyline points="20 12 20 22 4 22 4 12"></polyline>
+                                <rect x="2" y="7" width="20" height="5"></rect>
+                                <line x1="12" y1="22" x2="12" y2="7"></line>
+                                <path d="M12 7H7.5a2.5 2.5 0 0 1 0-5C11 2 12 7 12 7z"></path>
+                                <path d="M12 7h4.5a2.5 2.5 0 0 0 0-5C13 2 12 7 12 7z"></path>
+                            </svg>
                         </a>/ Settings
                     </h2>
                 </div>
-                <div class="top">
-                    <div class="left">
-                        <h2>Products available</h2>
-                        <p>products on demand</p>
+                <div class="set">
+                    <div class="view">
+                        <div class="txt">
+                            <h2>Account Overview</h2>
+                            <div class="line"></div>
+                        </div>
+                        <div class="row">
+                            <img src="assets/vick.png" alt="">
+                            <div class="container">
+
+                            </div>
+                        </div>
                     </div>
-                    <div class="center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
-                            class="feather feather-search">
-                            <circle cx="11" cy="11" r="8"></circle>
-                            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                        </svg>
-                        <input type="text" name="search" id="search" placeholder="e.g; biscuit">
-                    </div>
-                    <div class="right">
-                        <a href="brandForm.php">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-plus-square">
-                                <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
-                                <line x1="12" y1="8" x2="12" y2="16"></line>
-                                <line x1="8" y1="12" x2="16" y2="12"></line>
-                            </svg>
-                            Add product
-                        </a>
-                    </div>
-                </div>
-                <div class="down">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th scope="col">Product Id</th>
-                                <th scope="col">Product Name</th>
-                                <th scope="col">Brand</th>
-                                <th scope="col">Manufacturing Date</th>
-                                <th scope="col">Expiration Date</th>
-                                <th scope="col">Price</th>
-                                <th scope="col">Available</th>
-                                <th scope="col">Rating</th>
-                                <th scope="col">Actions</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <?php
-                            require("dbConnect.php");
-
-                            $con = new mysql(SERVER, USERNAME, PASSWORD, DATABASE);
-
-                            if($con->connet_error){
-                                die("Cannot connect to the database ".$con->connect_error);
-                            }else{
-                                $sql = "SELECT * FROM `Brands`;";
-
-                                $result = $con->query($sql);
-                                
-
-                                while($row = $result->fetch_assoc()){
-                                    echo "<tr scope='row'> 
-                                            <td>" .$row['Brand_ID']. "</td>
-                                            <td>" .$row['Brand_Name']. "</td>
-                                            <td>" .$row['Brand_Email']. "</td>
-                                            <td>" .$row['Brand_Contact']. "</td>
-                                            <td>" .$row['Brand_Status']. "</td>
-                                            <td>" .$row['Brand_Rating']. "</td>
-                                            <td>" .$row['Brand_Rating']. "</td>
-                                        </tr>";
-                                }
-                            }
-
-                        ?>
-                            <tr>
-                                <td>Yes</td>
-                            </tr>
-
-                        </tbody>
-                    </table>
                 </div>
             </main>
         </div>
