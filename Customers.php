b102d20019e5c95950ada454a58033b0231cf908
Victory Odumeh <vodumeh@gmail.com>
2023-07-01 17:06:42 +0100

    the aside bar is fine

--- Customers.php
+++ Customers.php
@@ -43,7 +43,7 @@
 
     <section class="customer">
         <div class="max-width">
-            <aside>
+            <aside style="width: 35%; height: 100vh;">
                 <div class=" x">
                     <svg class="x" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
                         fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
