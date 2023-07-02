2b5665a72c9e98429047be446c57cf4b40a04b4e
Victory Odumeh <vodumeh@gmail.com>
2023-07-02 09:49:26 +0100

    git is misbehaving now

--- Settings.php
+++ Settings.php
@@ -72,6 +72,17 @@
                             <a href="Brands.php">Password</a>
                         </li>
                         <li>
+                            <svg xmlns=" http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="feather feather-grid">
+                                <rect x="3" y="3" width="7" height="7"></rect>
+                                <rect x="14" y="3" width="7" height="7"></rect>
+                                <rect x="14" y="14" width="7" height="7"></rect>
+                                <rect x="3" y="14" width="7" height="7"></rect>
+                            </svg>
+                            <a href="index.php">Dashboard</a>
+                        </li>
+                        <li>
                             <i class="fa-solid fa-arrow-right-from-bracket fa-flip"></i>
                             <a href="#">Sign-out</a>
                         </li>
