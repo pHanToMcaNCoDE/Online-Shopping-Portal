11e466ab22fdfd1c88c484986ea419243760d649
Victory Odumeh <vodumeh@gmail.com>
2023-07-02 21:33:38 +0100

    moving to password page

--- Brands.php
+++ Brands.php
@@ -230,16 +230,6 @@
     </section>
 
     <script src="js/script.js"></script>
-    <script src="assets/sweetalert2/sweetalert2.min.js"></script>
-
-    <script>
-    Swal.fire(
-        'Good job!',
-        'You clicked the button!',
-        'success'
-    )
-    </script>
-
 </body>
 
 </html>
