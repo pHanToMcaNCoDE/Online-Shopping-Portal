54511d45cf6722289882ebaf2d1490d8f18be7bc
Victory Odumeh <vodumeh@gmail.com>
2023-07-02 22:06:57 +0100

    .

--- Settings.php
+++ Settings.php
@@ -51,25 +51,12 @@
                 <div class="content">
                     <ul class="aside-up">
                         <li class="current">
-                            <svg xmlns=" http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-grid">
-                                <rect x="3" y="3" width="7" height="7"></rect>
-                                <rect x="14" y="3" width="7" height="7"></rect>
-                                <rect x="14" y="14" width="7" height="7"></rect>
-                                <rect x="3" y="14" width="7" height="7"></rect>
-                            </svg>
-                            <a href="index.php">Account</a>
+                            <i class="fa-solid fa-user"></i>
+                            <a href="Settings.php">Account</a>
                         </li>
                         <li>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-star">
-                                <polygon
-                                    points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
-                                </polygon>
-                            </svg>
-                            <a href="Brands.php">Password</a>
+                            <i class="fa-solid fa-key"></i>
+                            <a href="Password.php">Password</a>
                         </li>
                         <li>
                             <svg xmlns=" http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
@@ -149,16 +136,6 @@
     </section>
 
     <script src="js/script.js"></script>
-    <script src="assets/sweetalert2/sweetalert2.min.js"></script>
-
-    <script>
-    Swal.fire(
-        'Good job!',
-        'You clicked the button!',
-        'success'
-    )
-    </script>
-
 </body>
 
 </html>
