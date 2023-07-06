48fa7e3e9c1ed0034b931c95c21d9101dcf11014
Victory Odumeh <vodumeh@gmail.com>
2023-07-06 11:11:32 +0100

    .

--- index.php
+++ index.php
@@ -39,8 +39,8 @@
                     <div class="num">1</div>
                 </li> -->
                 <div class="user">
-                    <img src="assets/vick.png" alt="username">
-                    <p>Victory Odumeh</p>
+                    <i class="fa-solid fa-arrow-right-from-bracket fa-flip"></i>
+                    <a href="#" style="margin-left: .3em; color: var(--bl);">Sign-out</a>
                 </div>
             </div>
         </nav>
@@ -63,7 +63,7 @@
                 </div>
                 <div class="content">
                     <ul class="aside-up">
-                        <li>
+                        <li class="current">
                             <svg xmlns=" http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="feather feather-grid">
@@ -116,7 +116,7 @@
                             </svg>
                             <a href="Customers.php">Customers</a>
                         </li>
-                        <li class="current">
+                        <li>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="feather feather-users">
@@ -129,7 +129,7 @@
                         </li>
                     </ul>
                     <ul class="aside-down">
-                        <li class="current">
+                        <li>
                             <i class="fa-solid fa-user"></i>
                             <a href="Account.php">Account</a>
                         </li>
