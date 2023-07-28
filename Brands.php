fcb81b5644616d2646fc5fdb9d16b55d0377d7ba
Victory Odumeh <vodumeh@gmail.com>
2023-07-28 08:04:07 +0100

    adding notifications and animations

--- Brands.php
+++ Brands.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopping Portal || Brands</title>
+    <title>Shopping Portal - Brands</title>
     <link rel="stylesheet" href="css/styles.css">
     <!-- <link rel="stylesheet" href="assets/sweetalert2/sweetalert2.min.css"> -->
     <script src="https://kit.fontawesome.com/032421aa45.js" crossorigin="anonymous"></script>
